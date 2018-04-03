b5feb6bbd850122d66b071a3a0aad0492620e8b0
Chen Rotem Levy <chen@rotemlevy.name>
2018-04-03 21:10:40 +0300

    playground: respoce

--- bs_api/playground.py
+++ bs_api/playground.py
@@ -1,4 +1,4 @@
-from apistar import http, Route
+from apistar import http, Include, Route, Response
 
 
 # Request
@@ -26,8 +26,26 @@ def show_user_agent(user_agent: http.Header):
     }
 
 
+# Response
+
+def create_project_default():
+    """Create an object with default return code 200 OK"""
+    return {'name': 'new project', 'id': 123}
+
+
+def create_project():
+    """Create a project with 201 CREATED status code
+
+    By returning a Response object
+    """
+    data = {'name': 'new project', 'id': 123}
+    headers = {'location': 'http://chen.rotemlevy.name/project/123'}
+    return Response(data, status=201, headers=headers)
 routes = [
     Route('show_request', 'GET', show_request),
     Route('show_query_params', 'GET', show_query_params),
     Route('show_user_agent', 'GET', show_user_agent),
+
+    Route('create_project_default', 'GET', create_project_default),
+    Route('create_project', 'GET', create_project),
 ]
